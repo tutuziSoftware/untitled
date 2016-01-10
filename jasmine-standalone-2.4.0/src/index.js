0e6d262bf8bde21fae3e7d26ad9804666dd70f03
ゆきかご <p.u.r.u.h.i.m.e@gmail.com>
2016-01-10 16:09:11 +0900

    jasmine追加

--- jasmine-standalone-2.4.0/src/index.js
+++ jasmine-standalone-2.4.0/src/index.js
@@ -62,6 +62,20 @@
             });
         }
 
+        /**
+         * 作業時間総数から出るタスクの添字番号を返します。
+         * @param work_time
+         */
+        get_out_of_task_index(work_time){
+            for(var i = 0 ; i != this.length ; i++){
+                work_time -= this.work_time;
+
+                if(work_time < 0){
+                    return i;
+                }
+            }
+        }
+
         save(){
             var task_list = this.map((task)=>{
                 return task.id;
